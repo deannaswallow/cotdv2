9e9ecc796aa6a2faad7d22f6a843f26477485457
Deanna Swallow <swallowdeanna@gmail.com>
2019-02-09 17:10:25 -0700

    added prop types

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { formatPrice } from '../helpers';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 class Order extends React.Component {
+  static propTypes = {
+    fishes: PropTypes.object,
+    order: PropTypes.object,
+    removeFromOrder: PropTypes.func
+  }
+
   renderOrder = key => {
     const fish = this.props.fishes[key];
     const count = this.props.order[key];
@@ -10,7 +17,7 @@ class Order extends React.Component {
     const transitionOptions = {
       classNames: "order",
       key: key,
-      timeout: { enter: 500, exit:500 }
+      timeout: { enter: 500, exit: 500 }
     };
     if(!fish) return null;
     if(!isAvailable) {
@@ -30,7 +37,8 @@ class Order extends React.Component {
               <CSSTransition
                 classNames="count"
                 key={count}
-                timeout={{ enter: 500, exit: 500 }}>
+                timeout={{ enter: 500, exit: 500 }}
+              >
                 <span>{count}</span>
               </CSSTransition>
             </TransitionGroup>
